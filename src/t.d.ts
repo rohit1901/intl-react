22b4a297e882eea5af068c618033899ee42c97b8
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-07-24 22:04:19 +0200

    feat: workflows
    - added workflows
    - prettier
    - eslint

--- src/t.d.ts
+++
@@ -1,5 +0,0 @@
-import { PropsWithChildren } from "react";
-import { IntlContext, IntlProps, UseTranslation } from "./models.ts";
-export declare const IntlReactContext: import("react").Context<IntlContext>;
-export declare function IntlReact({ children, languages, defaultLanguage, detectBrowserLanguage, }: PropsWithChildren<IntlProps>): import("react/jsx-runtime").JSX.Element;
-export declare function useTranslation(): UseTranslation;
