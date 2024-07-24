6c509db960a0e4d49381bc69fc54a8b09a22f761
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-07-24 22:30:57 +0200

    feat: workflows
    - added workflows
    - prettier
    - eslint
    - removed redundant declaration file

--- example/useAutocompleteT.ts
+++ example/useAutocompleteT.ts
@@ -1,12 +1,17 @@
-import { useTranslation as useTranslationIntl, Autocomplete, TParams, tr } from "../src";
+import {
+  useTranslation as useTranslationIntl,
+  Autocomplete,
+  TParams,
+  tr,
+} from "../src";
 import en from "../src/i18n/en.json";
 
 type Key = Autocomplete<typeof en>;
 
 export const useTranslation = () => {
-    const { locale, languages, defaultLanguage } = useTranslationIntl();
-    return {
-        T: (key: Key, params?: TParams) =>
-            tr({ locale, languages, defaultLanguage }, key, params),
-    };
+  const { locale, languages, defaultLanguage } = useTranslationIntl();
+  return {
+    T: (key: Key, params?: TParams) =>
+      tr({ locale, languages, defaultLanguage }, key, params),
+  };
 };
--- src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { tr } from "./tr.tsx";
-export * from "./models.ts";
-export { useTranslation, IntlReact, IntlReactContext } from "./t.tsx";
