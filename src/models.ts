574180d7a2413bd380114bd05215b4c0c6b41a0c
Rohit Khanduri <rohit.khanduri@hotmail.com>
2024-07-25 08:28:42 +0200

    fix: updated models
    - changed interfaces to types

--- src/models.ts
+++ src/models.ts
@@ -1,23 +1,18 @@
-export interface IntlProps {
+export type IntlProps = {
   languages: Record<string, any>;
   defaultLanguage: string;
   detectBrowserLanguage?: boolean;
-}
-
-export interface IntlContext {
+};
+export type IntlContext = {
   locale: string;
   setLocale: (language: string) => void;
   languages: Record<string, any>;
   defaultLanguage: string;
-}
-
+};
 export type TrContext = Omit<IntlContext, "setLocale">;
-
 type KeyPrefix<T extends string> = T extends "" ? "" : `.${T}`;
-
 type Suffix = "zero" | "one" | "two" | "few" | "many" | "female" | "male";
 type DynamicSuffix = Partial<Record<Suffix, string>>;
-
 export type KeyPath<T> = (
   T extends DynamicSuffix
     ? ""
@@ -29,14 +24,15 @@ export type KeyPath<T> = (
 ) extends infer D
   ? Extract<D, string>
   : never;
-
-export type TParams = { count?: number; [key: string]: any };
-
+export type TParams = {
+  count?: number;
+  [key: string]: any;
+};
 export type Autocomplete<schema> = KeyPath<schema>;
-
-export interface UseTranslation extends IntlContext {
+export type UseTranslation = {
   T: <Key extends string, Params extends TParams>(
     key: Key,
     params?: Params,
   ) => string;
-}
+} & IntlContext;
+export {};
